ee32d71287260f8326c49e6cea0cb30beabc3e94
KronembergerYgor <ygorkronemberger@gmail.com>
2024-09-11 22:43:54 -0300

    Definindo models
    Signed-off-by: KronembergerYgor <ygorkronemberger@gmail.com>

--- gestao_financas/app/Models/SpaceProject.php
+++ gestao_financas/app/Models/SpaceProject.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class SpaceProject extends Model
 {
     use HasFactory;
+
+    // Se você quiser especificar a tabela (opcional, mas não necessário se o nome seguir a convenção)
+    protected $table = 'space_projects';
+
+    // Se você não deseja que os campos timestamps sejam gerenciados automaticamente pelo Eloquent
+    public $timestamps = true;
+
+    // Se você quiser definir quais campos são atribuíveis em massa
+    protected $fillable = [
+        'name',
+        'description'
+    ];
 }
